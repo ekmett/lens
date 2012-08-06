e19c497dfe0d069facbbd138f9830bf6bc1a4896
nand <nand@lavabit.com>
2012-08-06 17:35:40 +0200

    Update Pong.hs and delete old Pong2.hs

--- examples/Pong.hs
+++ examples/Pong.hs
@@ -1,27 +1,29 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TemplateHaskell, Rank2Types #-}
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Lens
-import Control.Lens.TH
+import Control.Lens.TH (makeLenses)
 import Control.Monad.State
 
 import Data.Set (Set, member, empty, insert, delete)
+import Data.Pair.Lens (both)
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+
+import System.Random
 
 -- Some global constants
 
 gameSize      = 300
 windowSize    = 480
 ballRadius    = 0.02
-initialSpeed  = (0.8, 0.3)
 speedIncrease = 1.1
+initialSpeed  = 0.5
 paddleWidth   = 0.02
 paddleHeight  = 0.3
 paddleSpeed   = 1
-textSize      = 0.3 / gameSize
+textSize      = 0.001
 
 -- Pure data type for representing the game state
 
@@ -31,24 +33,37 @@ data Pong = Pong
   , _paddle1   :: Float
   , _paddle2   :: Float
   , _score     :: (Int, Int)
+  , _vectors   :: [Vector]
 
   -- Since gloss doesn't cover this, we store the set of pressed keys
   , _keys      :: Set Key
   }
 
-initial :: Pong
-initial = Pong (0, 0) initialSpeed 0 0 (0, 0) empty
-
 -- Some nice lenses to go with it
 makeLenses ''Pong
 
--- I'm just renaming the tuple lenses for enhanced clarity with points/vectors
+-- Renamed tuple lenses for enhanced clarity with points/vectors
 _x = _1
 _y = _2
 
--- This will be in Data.Pair.Lens soon
-both :: Traversal (a,a) (b,b) a b
-both f (x,y) = (,) <$> f x <*> f y
+initial :: Pong
+initial = Pong (0, 0) (0, 0) 0 0 (0, 0) [] empty
+
+-- Calculate the y position at which the ball will next hit (on player2's side)
+hitPos :: Point -> Vector -> Float
+hitPos (x,y) (u,v) = ypos
+  where
+    xdist = if u >= 0 then 1 - x else 3 + x
+    time  = xdist / abs u
+    ydist = v * time
+    ypos  = bounce (y + ydist)
+    o     = 1 - ballRadius
+
+    -- Calculate bounces iteratively
+    bounce n
+      | n >  o    = bounce (  2 *o - n)
+      | n < -o    = bounce ((-2)*o - n)
+      | otherwise = n
 
 -- Game update logic
 
@@ -61,46 +76,42 @@ update time = execState $ do
 -- Move the ball by adding its current speed
 updateBall :: Float -> State Pong ()
 updateBall time = do
-  speed <- use ballSpeed
-  ballPos += speed `mul` time
+  (u, v) <- use ballSpeed
+  ballPos += (time * u, time * v)
 
   -- Make sure it doesn't leave the playing area
-  ballPos.both %= clampPad ballRadius
-
- where
-   (a,b) `mul` c = (c*a, c*b)
-   infixl 7 `mul`
+  ballPos.both %= clamp ballRadius
 
 -- Update the paddles
 updatePaddles :: Float -> State Pong ()
 updatePaddles time = do
   p <- get
 
+  let paddleMovement = time * paddleSpeed
+  let keyPressed key = SpecialKey key `member` (p^.keys)
+
   -- Update the player's paddle based on keys
-  when (SpecialKey KeyUp `isIn` p^.keys) $
-    paddle1 += paddleSpeed * time
+  when (keyPressed KeyUp)   $ paddle1 += paddleMovement
+  when (keyPressed KeyDown) $ paddle1 -= paddleMovement
 
-  when (SpecialKey KeyDown `isIn` p^.keys) $
-    paddle1 -= paddleSpeed * time
+  -- Calculate the optimal position
+  let optimal = hitPos (p^.ballPos) (p^.ballSpeed)
+  let dist    = optimal - p^.paddle2
 
-  -- Update the CPU's paddle based on the ball's relative position
-  case compare (p^.ballPos._y) (p^.paddle2) of
-    GT -> paddle2 += paddleSpeed * time
-    LT -> paddle2 -= paddleSpeed * time
-    _  -> return ()
+  -- Move the CPU's paddle towards this optimal position as needed
+  when (abs dist > paddleHeight/3) $
+    case compare dist 0 of
+      GT -> paddle2 += paddleMovement
+      LT -> paddle2 -= paddleMovement
+      _  -> return ()
 
   -- Make sure both paddles don't leave the playing area
-  paddle1 %= clamp
-  paddle2 %= clamp
-
- where
-   clamp = clampPad (paddleHeight/2)
-   isIn  = member
-   infixl 7 `isIn`
+  paddle1 %= clamp (paddleHeight/2)
+  paddle2 %= clamp (paddleHeight/2)
 
 -- Clamp to the region (-1, 1) but with padding
-clampPad :: Float -> Float -> Float
-clampPad pad = max (pad - 1) . min (1 - pad)
+clamp :: Float -> Float -> Float
+clamp pad = max (pad - 1) . min (1 - pad)
 
 -- Check for collisions and/or scores
 checkBounds :: State Pong ()
@@ -109,63 +120,64 @@ checkBounds = do
   let (x,y) = p^.ballPos
 
   -- Check for collisions with the top or bottom
-  when (abs y >= top) $
+  when (abs y >= edge) $
     ballSpeed._y %= negate
 
   -- Check for collisions with paddles
-  let check l = y >= p^.l - paddleHeight/2 && y <= p^.l + paddleHeight/2
-      collide = do
+  let { check paddle other =
+    if y >= p^.paddle - paddleHeight/2 && y <= p^.paddle + paddleHeight/2
+      then do
         ballSpeed._x   %= negate
         ballSpeed.both *= speedIncrease
 
-  when (x <= left) $
-    if check paddle1
-      then collide
       else do
-        score._2 += 1
+        score.other += 1
         reset
+  }
 
-  when (x >= right) $
-    if check paddle2
-      then collide
-      else do
-        score._1 += 1
-        reset
+  when (x >=  edge) $ check paddle2 _1
+  when (x <= -edge) $ check paddle1 _2
 
   where
-    top   = 1 - ballRadius
-    left  = ballRadius + paddleWidth/2 - 1
-    right = -left
+    edge = 1 - ballRadius
 
 -- Reset the game
 reset :: State Pong ()
 reset = do
-  ballPos   ^= initial^.ballPos
-  ballSpeed ^= initial^.ballSpeed
+  ballPos ^= (0, 0)
+  (ballSpeed ^=) =<< nextSpeed
+
+-- Retrieve a speed from the list, dropping it in the process
+nextSpeed :: State Pong Vector
+nextSpeed = do
+  v:vs <- use vectors
+  vectors ^= vs
+  return v
 
 -- Drawing a pong state to the screen
 
 draw :: Pong -> Picture
 draw p = scale gameSize gameSize $ Pictures
   [ drawBall   `at` p^.ballPos
-  , drawPaddle `at` (-1, p^.paddle1)
-  , drawPaddle `at` ( 1, p^.paddle2)
+  , drawPaddle `at` (-paddleX, p^.paddle1)
+  , drawPaddle `at` ( paddleX, p^.paddle2)
 
   -- Score and playing field
-  , scale textSize textSize (p^.score.pretty.to text) `at` (-0.1, 0.85)
+  , drawScore (p^.score) `at` (-0.1, 0.85)
   , rectangleWire 2 2
   ]
   where
-    -- Pretty printing lens
-    pretty = to (\(x,y) -> show x ++ " " ++ show y)
-    p `at` (x,y) = translate x y p
-    infixr 1 `at`
+    paddleX = 1 + paddleWidth/2
+    p `at` (x,y) = translate x y p; infixr 1 `at`
 
 drawPaddle :: Picture
 drawPaddle = rectangleSolid paddleWidth paddleHeight
 
 drawBall :: Picture
 drawBall = circleSolid ballRadius
+
+drawScore :: (Int, Int) -> Picture
+drawScore (x, y) = scale textSize textSize . text $ show x ++ " " ++ show y
 
 -- Handle input by simply updating the keys set
 
@@ -176,9 +188,24 @@ handle _ = id
 
 -- The main program action
 
-main = play display backColor fps initial draw handle update
+main = do
+  v:vs <- startingSpeeds
+  let world = ballSpeed <~ v $ vectors <~ vs $ initial
+  play display backColor fps world draw handle update
+
   where
-    -- display   = InWindow "Pong!" (windowSize, windowSize) (800, 600)
-    display   = FullScreen (800,600)
+    display   = InWindow "Pong!" (windowSize, windowSize) (200, 200)
     backColor = white
     fps       = 120
+
+-- Generate the random list of starting speeds
+
+startingSpeeds :: IO [Vector]
+startingSpeeds = do
+  rs <- randomRs (-initialSpeed, initialSpeed) <$> getStdGen
+  return . interleave $ filter ((> 0.2) . abs) rs
+
+  where
+    interleave :: [a] -> [(a,a)]
+    interleave (x:y:xs) = (x,y) : interleave xs
+    interleave _        = []
