9d0127eac298440cf96daf1ab86b25883dd9d36b
Edward Kmett <ekmett@gmail.com>
2012-09-03 22:14:27 -0400

    goodnight gracie

--- src/Control/Lens/Setter.hs
+++ src/Control/Lens/Setter.hs
@@ -316,7 +316,7 @@ set l d = runMutator . l (\_ -> Mutator d)
 --
 -- >>> import Data.Map as Map
 -- >>> _2.at "hello" <.~ Just "world" $ (42,Map.fromList [("goodnight","gracie")])
--- (Just "world",(42,fromList [("hello","world"),("goodnight","gracie")]))
+-- (Just "world",(42,fromList [("goodnight","gracie"),("hello","world")]))
 --
 -- @
 -- ('<.~') :: 'Setter' a b c d    -> d -> a -> (d, b)
