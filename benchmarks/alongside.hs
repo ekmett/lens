2e096a49ffca7d90730d175e54f8f1039f9c3093
Edward Kmett <ekmett@gmail.com>
2012-09-10 01:43:50 -0400

    added alongside benchmarks to benchmark suite (#44). added a final encoding of Context to the suite as well, but haven't wired it up yet

--- benchmarks/alongside.hs
+++ benchmarks/alongside.hs
@@ -1,6 +1,41 @@
 {-# LANGUAGE RankNTypes #-}
-import           Control.Lens
-import           Criterion.Main
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+import Control.Applicative
+import Control.Comonad
+import Control.Comonad.Store.Class
+import Control.Lens
+import Criterion.Main
+import Data.Functor.Compose
+import Data.Functor.Identity
+
+-- | A finally encoded Store
+newtype Experiment c d a = Experiment { runExperiment :: forall f. Functor f => (c -> f d) -> f a }
+
+instance Functor (Experiment c d) where
+  fmap f (Experiment k) = Experiment (fmap f . k)
+
+instance (c ~ d) => Comonad (Experiment c d) where
+  extract (Experiment m) = runIdentity (m Identity)
+  {-# INLINE extract #-}
+  duplicate = duplicateExperiment
+  {-# INLINE duplicate #-}
+
+-- | 'Experiment' is an indexed 'Comonad'.
+duplicateExperiment :: Experiment c e a -> Experiment c d (Experiment d e a)
+duplicateExperiment (Experiment m) = getCompose (m (Compose . fmap placebo . placebo))
+{-# INLINE duplicateExperiment #-}
+
+-- | A trivial 'Experiment'.
+placebo :: c -> Experiment c d d
+placebo i = Experiment (\k -> k i)
+
+instance (c ~ d) => ComonadStore c (Experiment c d) where
+  pos m = getConst (runExperiment m Const)
+  peek d m = runIdentity $ runExperiment m (\_ -> Identity d)
+  peeks f m = runIdentity $ runExperiment m (\c -> Identity (f c))
+  experiment cfd m = runExperiment m cfd
 
 compound :: Lens a b c d
          -> Lens a' b' c' d'
