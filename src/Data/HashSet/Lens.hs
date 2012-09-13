fa2cacf543765204ca6ac156d5dadd14fd08854b
Edward Kmett <ekmett@gmail.com>
2012-09-13 00:05:37 -0400

    hyperlinking

--- src/Data/HashSet/Lens.hs
+++ src/Data/HashSet/Lens.hs
@@ -26,7 +26,7 @@ import Data.Hashable
 -- | This 'Setter' can be used to change the type of a 'HashSet' by mapping
 -- the elements to new values.
 --
--- Sadly, you can't create a valid 'Traversal' for a 'Set', but you can
+-- Sadly, you can't create a valid 'Control.Lens.Traversal.Traversal' for a 'Set', but you can
 -- manipulate it by reading using 'folded' and reindexing it via 'setmap'.
 --
 -- >>> over setmapped (+1) (fromList [1,2,3,4])
@@ -35,17 +35,17 @@ setmapped :: (Eq i, Hashable i, Eq j, Hashable j) => Setter (HashSet i) (HashSet
 setmapped = sets HashSet.map
 {-# INLINE setmapped #-}
 
--- | Construct a set from a 'Getter', 'Fold', 'Traversal', 'Lens' or 'Iso'.
+-- | Construct a set from a 'Getter', 'Control.Lens.Fold.Fold', 'Control.Lens.Traversal.Traversal', 'Control.Lens.Type.Lens' or 'Control.Lens.Iso.Iso'.
 --
 -- >>> setOf (folded._2) [("hello",1),("world",2),("!!!",3)]
 -- fromList [1,2,3]
 --
 -- @
 -- setOf :: 'Hashable' c         => 'Getter' a c           -> a -> 'HashSet' c
--- setOf :: ('Eq' c, 'Hashable' c) => 'Fold' a c             -> a -> 'HashSet' c
--- setOf :: 'Hashable' c         => 'Simple' 'Iso' a c       -> a -> 'HashSet' c
--- setOf :: 'Hashable' c         => 'Simple' 'Lens' a c      -> a -> 'HashSet' c
--- setOf :: ('Eq' c, 'Hashable' c) => 'Simple' 'Traversal' a c -> a -> 'HashSet' c
+-- setOf :: ('Eq' c, 'Hashable' c) => 'Control.Lens.Fold.Fold' a c             -> a -> 'HashSet' c
+-- setOf :: 'Hashable' c         => 'Control.Lens.Type.Simple' 'Control.Lens.Iso.Iso' a c       -> a -> 'HashSet' c
+-- setOf :: 'Hashable' c         => 'Control.Lens.Type.Simple' 'Control.Lens.Type.Lens' a c      -> a -> 'HashSet' c
+-- setOf :: ('Eq' c, 'Hashable' c) => 'Control.Lens.Type.Simple' 'Control.Lens.Traversal.Traversal' a c -> a -> 'HashSet' c
 -- @
 setOf :: Hashable c => Getting (HashSet c) a b c d -> a -> HashSet c
 setOf l = runAccessor . l (Accessor . HashSet.singleton)
--- src/Data/Set/Lens.hs
+++ src/Data/Set/Lens.hs
@@ -25,7 +25,7 @@ import Data.Set as Set
 -- | This 'Setter' can be used to change the type of a 'Set' by mapping
 -- the elements to new values.
 --
--- Sadly, you can't create a valid 'Traversal' for a 'Set', but you can
+-- Sadly, you can't create a valid 'Control.Lens.Traversal.Traversal' for a 'Set', but you can
 -- manipulate it by reading using 'folded' and reindexing it via 'setmap'.
 --
 -- >>> over setmapped (+1) (fromList [1,2,3,4])
@@ -34,17 +34,17 @@ setmapped :: (Ord i, Ord j) => Setter (Set i) (Set j) i j
 setmapped = sets Set.map
 {-# INLINE setmapped #-}
 
--- | Construct a set from a 'Getter', 'Fold', 'Traversal', 'Lens' or 'Iso'.
+-- | Construct a set from a 'Getter', 'Control.Lens.Fold.Fold', 'Control.Lens.Traversal.Traversal', 'Control.Lens.Type.Lens' or 'Control.Lens.Iso.Iso'.
 --
 -- >>> setOf (folded._2) [("hello",1),("world",2),("!!!",3)]
 -- fromList [1,2,3]
 --
 -- @
 -- setOf ::          'Getter' a c           -> a -> 'Set' c
--- setOf :: 'Ord' c => 'Fold' a c             -> a -> 'Set' c
--- setOf ::          'Simple' 'Iso' a c       -> a -> 'Set' c
--- setOf ::          'Simple' 'Lens' a c      -> a -> 'Set' c
--- setOf :: 'Ord' c => 'Simple' 'Traversal' a c -> a -> 'Set' c
+-- setOf :: 'Ord' c => 'Control.Lens.Fold.Fold' a c             -> a -> 'Set' c
+-- setOf ::          'Control.Lens.Type.Simple' 'Control.Lens.Iso.Iso' a c       -> a -> 'Set' c
+-- setOf ::          'Control.Lens.Type.Simple' 'Control.Lens.Type.Lens' a c      -> a -> 'Set' c
+-- setOf :: 'Ord' c => 'Control.Lens.Type.Simple' 'Control.Lens.Traversal.Traversal' a c -> a -> 'Set' c
 -- @
 setOf :: Getting (Set c) a b c d -> a -> Set c
 setOf l = runAccessor . l (Accessor . Set.singleton)
