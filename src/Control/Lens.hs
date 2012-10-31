d4f5773b570edbdc50ddddcc03255f9afe29f5db
Edward Kmett <ekmett@gmail.com>
2012-10-31 03:36:37 -0400

    Updated links to 3.1 Hierarchy

--- src/Control/Lens.hs
+++ src/Control/Lens.hs
@@ -41,7 +41,7 @@
 --
 -- <http://github.com/ekmett/lens/wiki>
 --
--- <<http://github.com/ekmett/lens/wiki/images/Hierarchy-3.0.png>>
+-- <<http://github.com/ekmett/lens/wiki/images/Hierarchy-3.1.png>>
 ----------------------------------------------------------------------------
 module Control.Lens
   ( module Control.Lens.Type
