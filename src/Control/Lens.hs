b72a5bf171f5b0a0848109b9cdb356aeddee8952
Edward Kmett <ekmett@gmail.com>
2012-11-22 21:12:08 -0500

    3.6 hierarchy link

--- src/Control/Lens.hs
+++ src/Control/Lens.hs
@@ -44,7 +44,7 @@
 --
 -- <http://github.com/ekmett/lens/wiki>
 --
--- <<http://github.com/ekmett/lens/wiki/images/Hierarchy-3.5.png>>
+-- <<http://github.com/ekmett/lens/wiki/images/Hierarchy-3.6.png>>
 ----------------------------------------------------------------------------
 module Control.Lens
   ( module Control.Lens.Type
