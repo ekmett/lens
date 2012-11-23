d3b622e61c96e1ab5118b03aa4c8cfee91d0e346
Shachaf Ben-Kiki <shachaf@gmail.com>
2012-11-23 04:11:10 -0800

    Added import for doctest

--- src/Data/Data/Lens.hs
+++ src/Data/Data/Lens.hs
@@ -58,6 +58,9 @@ import           GHC.IO
 import           GHC.Exts (realWorld#)
 import           Unsafe.Coerce as Unsafe
 
+-- $setup
+-- >>> import Control.Lens.Setter
+
 -------------------------------------------------------------------------------
 -- Generic Traversal
 -------------------------------------------------------------------------------
