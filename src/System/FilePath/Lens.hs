c87c5e97549ea905d642394b8681e00cded5ab72
Edward Kmett <ekmett@gmail.com>
2012-11-24 01:35:20 -0500

    filepath tests

--- src/System/FilePath/Lens.hs
+++ src/System/FilePath/Lens.hs
@@ -16,7 +16,7 @@ module System.FilePath.Lens
 
 import Control.Applicative ((<$>))
 
-import Control.Monad.State.Class as State
+import Control.Monad.State as State
 import System.FilePath
   ( (</>), (<.>), splitExtension
   , takeBaseName, takeDirectory
@@ -30,9 +30,8 @@ infix 4 </>=, <</>=, <.>=, <<.>=
 
 -- | Modify the path by adding another path.
 --
--- >>> :m + Control.Lens
--- >>> both </>~ "!!!" $ ("hello","world")
--- ("hello/!!!","world/!!!")
+-- >>> both </>~ "bin" $ ("hello","world")
+-- ("hello/bin","world/bin")
 --
 -- @
 -- ('</>~') :: 'Setter' s a 'FilePath' 'FilePath' -> 'FilePath' -> s -> a
@@ -46,6 +45,9 @@ l </>~ n = over l (</> n)
 
 
 -- | Modify the target(s) of a 'Simple' 'Lens', 'Iso', 'Setter' or 'Traversal' by adding a path.
+--
+-- >>> execState (both </>= "bin") ("hello","world")
+-- ("hello/bin","world/bin")
 --
 -- @
 -- ('</>=') :: 'MonadState' s m => 'Simple' 'Setter' s 'FilePath' -> 'FilePath' -> m ()
@@ -77,9 +79,8 @@ l <</>= r = l <%= (</> r)
 
 -- | Modify the path by adding extension.
 --
--- >>> :m + Control.Lens
--- >>> both <.>~ "!!!" $ ("hello","world")
--- ("hello.!!!","world.!!!")
+-- >>> both <.>~ "txt" $ ("hello","world")
+-- ("hello.txt","world.txt")
 --
 -- @
 -- ('<.>~') :: 'Setter' s a 'FilePath' 'FilePath' -> 'String' -> s -> a
@@ -91,8 +92,10 @@ l <</>= r = l <%= (</> r)
 l <.>~ n = over l (<.> n)
 {-# INLINE (<.>~) #-}
 
-
 -- | Modify the target(s) of a 'Simple' 'Lens', 'Iso', 'Setter' or 'Traversal' by adding an extension.
+--
+-- >>> execState (both <.>= "txt") ("hello","world")
+-- ("hello.txt","world.txt")
 --
 -- @
 -- ('<.>=') :: 'MonadState' s m => 'Simple' 'Setter' s 'FilePath' -> 'String' -> m ()
@@ -104,8 +107,10 @@ l <.>~ n = over l (<.> n)
 l <.>= b = State.modify (l <.>~ b)
 {-# INLINE (<.>=) #-}
 
-
 -- | Add an extension onto the end of the target of a 'Lens' and return the result
+--
+-- >>> _1 <<.>~ "txt" $ ("hello","world")
+-- ("hello.txt",("hello.txt","world"))
 --
 -- When you do not need the result of the operation, ('<.>~') is more flexible.
 (<<.>~) :: LensLike ((,)FilePath) s a FilePath FilePath -> String -> s -> (FilePath, a)
@@ -116,13 +121,16 @@ l <<.>~ m = l <%~ (<.> m)
 -- | Add an extension onto the end of the target of a 'Lens' into
 -- your monad's state and return the result.
 --
+-- >>> evalState (_1 <<.>= "txt") $("hello","world")
+-- "hello.txt"
+--
 -- When you do not need the result of the operation, ('<.>=') is more flexible.
 (<<.>=) :: MonadState s m => SimpleLensLike ((,)FilePath) s FilePath -> String -> m FilePath
 l <<.>= r = l <%= (<.> r)
 {-# INLINE (<<.>=) #-}
 
 
--- | A lens reading and writing to the basename.
+-- | A 'Lens' for reading and writing to the basename
 --
 -- >>> basename .~ "filename" $ "path/name.png"
 -- "path/filename.png"
@@ -131,7 +139,7 @@ basename f p = (<.> takeExtension p) . (takeDirectory p </>) <$> f (takeBaseName
 {-# INLINE basename #-}
 
 
--- | A lens reading and writing to the directory.
+-- | A 'Lens' for reading and writing to the directory
 --
 -- >>> "long/path/name.txt" ^. directory
 -- "long/path"
@@ -140,7 +148,7 @@ directory f p = (</> takeFileName p) <$> f (takeDirectory p)
 {-# INLINE directory #-}
 
 
--- | A lens reading and writing to the extension.
+-- | A 'Lens' for reading and writing to the extension
 --
 -- >>> extension .~ ".png" $ "path/name.txt"
 -- "path/name.png"
@@ -151,7 +159,7 @@ extension f p = (n <.>) <$> f e
 {-# INLINE extension #-}
 
 
--- | A lens reading and writing to the full filename.
+-- | A 'Lens' for reading and writing to the full filename
 --
 -- >>> filename .~ "name.txt" $ "path/name.png"
 -- "path/name.txt"
