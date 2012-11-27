6ee33b17287c4922cd533b6159f3b7716a153401
Edward Kmett <ekmett@gmail.com>
2012-11-27 12:33:33 -0500

    Removed unusable Wrapped instances.

--- src/Control/Lens/Wrapped.hs
+++ src/Control/Lens/Wrapped.hs
@@ -26,8 +26,8 @@ import           Control.Applicative
 import           Control.Applicative.Backwards
 import           Control.Applicative.Lift
 import           Control.Arrow
-import           Control.Comonad.Trans.Env
-import           Control.Comonad.Trans.Store
+-- import        Control.Comonad.Trans.Env
+-- import        Control.Comonad.Trans.Store
 import           Control.Comonad.Trans.Traced
 import           Control.Lens.Iso
 import           Control.Monad.Trans.Cont
@@ -196,17 +196,10 @@ instance Wrapped (Either (f a) (g a)) (Either (f' a') (g' a')) (Coproduct f g a)
   wrapped   = isos Coproduct getCoproduct Coproduct getCoproduct
   unwrapped = isos getCoproduct Coproduct getCoproduct Coproduct
 
-instance Wrapped (e, w a) (e', w' a') (EnvT e w a) (EnvT e' w' a') where
-  wrapped   = isos (uncurry EnvT) runEnvT (uncurry EnvT) runEnvT
-  unwrapped = isos runEnvT (uncurry EnvT) runEnvT (uncurry EnvT)
-
-instance Wrapped (w (s -> a), s) (w' (s' -> a'), s') (StoreT s w a) (StoreT s' w' a') where
-  wrapped   = isos (uncurry StoreT) runStoreT (uncurry StoreT) runStoreT
-  unwrapped = isos runStoreT (uncurry StoreT) runStoreT (uncurry StoreT)
-
 instance Wrapped (w (m -> a)) (w' (m' -> a')) (TracedT m w a) (TracedT m' w' a') where
   wrapped   = isos TracedT runTracedT TracedT runTracedT
   unwrapped = isos runTracedT TracedT runTracedT TracedT
+
 
 
 getArrowMonad :: ArrowApply m  => ArrowMonad m a -> m () a
