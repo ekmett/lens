810ab917ef01ddaede4b2378b5a022a48b624fca
Edward Kmett <ekmett@gmail.com>
2012-11-27 01:59:32 -0500

    haddock & style

--- src/Control/Lens/Projection.hs
+++ src/Control/Lens/Projection.hs
@@ -58,7 +58,7 @@ instance Projective Project where
   projecting = Project
 
 instance Isomorphic Project where
-  isos sa _ _ bt = Project bt (\afb s -> bt <$> afb (sa s))
+  isos sa _ _ bt = Project bt $ \afb s -> bt <$> afb (sa s)
 
 -- | A 'Projection' is a 'Traversal' that can also be turned around with 'by' to obtain a 'Getter'
 type Projection s t a b = forall k f. (Projective k, Applicative f) => k (a -> f b) (s -> f t)
@@ -91,7 +91,7 @@ by (Project bt _) = to (unsafeCoerce bt)
 -- >>> Left "hello" ^._left
 -- "hello"
 --
--- @_left :: 'Applicative' f => (a -> f b) -> 'Either' a c -> f ('Either' b c)@
+-- @'_left' :: 'Applicative' f => (a -> f b) -> 'Either' a c -> f ('Either' b c)@
 _left :: Projection (Either a c) (Either b c) a b
 _left = projecting Left $ \ f e -> case e of
   Left a  -> Left <$> f a
@@ -118,7 +118,7 @@ _left = projecting Left $ \ f e -> case e of
 -- >>> Left "hello" ^._right :: [Double]
 -- []
 --
--- @_right :: 'Applicative' f => (a -> f b) -> 'Either' c a -> f ('Either' c a)@
+-- @'_right' :: 'Applicative' f => (a -> f b) -> 'Either' c a -> f ('Either' c a)@
 _right :: Projection (Either c a) (Either c b) a b
 _right = projecting Right $ \f e -> case e of
   Left c -> pure $ Left c
