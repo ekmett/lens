54eede611ff3ffd32e3763c3d3526560b42d5f81
Edward Kmett <ekmett@gmail.com>
2012-12-05 13:15:18 -0500

    safer Data.Typeable.Lens

--- src/Data/Typeable/Lens.hs
+++ src/Data/Typeable/Lens.hs
@@ -21,30 +21,18 @@ module Data.Typeable.Lens
 import Control.Applicative
 import Control.Lens
 import Data.Typeable
-#ifndef SAFE
-import Unsafe.Coerce as Unsafe
-#else
 import Data.Maybe
-#endif
 
 -- | A 'Simple' 'Traversal' for working with a 'cast' of a 'Typeable' value.
 _cast :: (Typeable s, Typeable a) => Simple Traversal s a
 _cast f s = case cast s of
-#ifndef SAFE
-  Just a  -> Unsafe.unsafeCoerce <$> f a
-#else
   Just a  -> fromMaybe (error "_cast: recast failed") . cast <$> f a
-#endif
   Nothing -> pure s
 {-# INLINE _cast #-}
 
 -- | A 'Simple' 'Traversal' for working with a 'gcast' of a 'Typeable' value.
 _gcast :: (Typeable s, Typeable a) => Simple Traversal (c s) (c a)
 _gcast f s = case gcast s of
-#ifndef SAFE
-  Just a  -> Unsafe.unsafeCoerce <$> f a
-#else
   Just a  -> fromMaybe (error "_gcast: recast failed") . gcast <$> f a
-#endif
   Nothing -> pure s
 {-# INLINE _gcast #-}
