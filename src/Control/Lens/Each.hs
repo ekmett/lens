cb243210cff74b021c6748f04d95b7d8b8a57ced
Edward Kmett <ekmett@gmail.com>
2012-12-10 06:46:57 -0500

    INLINE each

--- src/Control/Lens/Each.hs
+++ src/Control/Lens/Each.hs
@@ -35,41 +35,54 @@ class Each i s t a b | s -> i a, t -> i b, s b -> t, t a -> s where
   each :: Traversal s t a b
   default each :: (Traversable f, s ~ f a, t ~ f b) => IndexedTraversal Int s t a b
   each = traversed
+  {-# INLINE each #-}
 
 instance (a ~ a', b ~ b') => Each Int (a,a') (b,b') a b where
   each = indexed $ \ f ~(a,b) -> (,) <$> f (0 :: Int) a <*> f 1 b
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, b ~ b2, b ~ b3) => Each Int (a,a2,a3) (b,b2,b3) a b where
   each = indexed $ \ f ~(a,b,c) -> (,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, b ~ b2, b ~ b3, b ~ b4) => Each Int (a,a2,a3,a4) (b,b2,b3,b4) a b where
   each = indexed $ \ f ~(a,b,c,d) -> (,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, b ~ b2, b ~ b3, b ~ b4, b ~ b5) => Each Int (a,a2,a3,a4,a5) (b,b2,b3,b4,b5) a b where
   each = indexed $ \ f ~(a,b,c,d,e) -> (,,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d <*> f 4 e
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6) => Each Int (a,a2,a3,a4,a5,a6) (b,b2,b3,b4,b5,b6) a b where
   each = indexed $ \ f ~(a,b,c,d,e,g) -> (,,,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d <*> f 4 e <*> f 5 g
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7) => Each Int (a,a2,a3,a4,a5,a6,a7) (b,b2,b3,b4,b5,b6,b7) a b where
   each = indexed $ \ f ~(a,b,c,d,e,g,h) -> (,,,,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d <*> f 4 e <*> f 5 g <*> f 6 h
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, a ~ a8, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8) => Each Int (a,a2,a3,a4,a5,a6,a7,a8) (b,b2,b3,b4,b5,b6,b7,b8) a b where
   each = indexed $ \ f ~(a,b,c,d,e,g,h,i) -> (,,,,,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d <*> f 4 e <*> f 5 g <*> f 6 h <*> f 7 i
+  {-# INLINE each #-}
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, a ~ a8, a ~ a9, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8, b ~ b9) => Each Int (a,a2,a3,a4,a5,a6,a7,a8,a9) (b,b2,b3,b4,b5,b6,b7,b8,b9) a b where
   each = indexed $ \ f ~(a,b,c,d,e,g,h,i,j) -> (,,,,,,,,) <$> f (0 :: Int) a <*> f 1 b <*> f 2 c <*> f 3 d <*> f 4 e <*> f 5 g <*> f 6 h <*> f 7 i <*> f 8 j
+  {-# INLINE each #-}
 
 instance Each Int [a] [b] a b
 
 instance Each Int StrictT.Text StrictT.Text Char Char where
   each = iso StrictT.unpack StrictT.pack .> traversed
+  {-# INLINE each #-}
 
 instance Each Int LazyT.Text LazyT.Text Char Char where
   each = iso LazyT.unpack LazyT.pack .> traversed
+  {-# INLINE each #-}
 
 instance Each Int StrictB.ByteString StrictB.ByteString Word8 Word8 where
   each = iso StrictB.unpack StrictB.pack .> traversed
+  {-# INLINE each #-}
 
 instance Each Int LazyB.ByteString LazyB.ByteString Word8 Word8 where
   each = iso LazyB.unpack LazyB.pack .> traversed
+  {-# INLINE each #-}
