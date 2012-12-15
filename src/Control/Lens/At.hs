0262a3d590f7d45cf61362c992b464080dea15c7
Edward Kmett <ekmett@gmail.com>
2012-12-15 05:16:28 -0500

    Added `ElKey` to fix defaulting.

--- src/Control/Lens/At.hs
+++ src/Control/Lens/At.hs
@@ -57,13 +57,15 @@ import Data.Vector as Vector hiding (indexed)
 
 
 -- | A deprecated alias for 'el'
-_at :: El k m => k -> SimpleIndexedTraversal k (m v) v
+_at :: El m => ElKey m -> SimpleIndexedTraversal (ElKey m) (m v) v
 _at = el
 {-# DEPRECATED _at "use 'el'. '_at' will be removed in version 3.9" #-}
 
 -- | This simple indexed traversal lets you 'traverse' the value at a given key in a map or element at an ordinal
 -- position in a list or sequence.
-class El k m | m -> k where
+class El m where
+  -- | What is the index type?
+  type ElKey m :: * 
   -- | This simple indexed traversal lets you 'traverse' the value at a given key in a map.
   --
   -- *NB:* _setting_ the value of this 'Traversal' will only set the value in the lens
@@ -79,14 +81,14 @@ class El k m | m -> k where
   --
   -- >>> Seq.fromList [a,b,c,d] ^? el 2
   -- Just c
-  el :: k -> SimpleIndexedTraversal k (m v) v
+  el :: ElKey m -> SimpleIndexedTraversal (ElKey m) (m v) v
 #ifdef DEFAULT_SIGNATURES
-  default el :: At k m => k -> SimpleIndexedTraversal k (m v) v
+  default el :: At m => ElKey m -> SimpleIndexedTraversal (ElKey m) (m v) v
   el k = at k <. traverse
 #endif
 
-
-instance El Int [] where
+instance El [] where
+  type ElKey [] = Int
   el k = indexed $ \ f xs0 ->
     let go [] _ = pure []
         go (a:as) 0 = f k a <&> (:as)
@@ -94,26 +96,30 @@ instance El Int [] where
     in go xs0 k
   {-# INLINE el #-}
 
-instance El Int Seq where
+instance El Seq where
+  type ElKey Seq = Int
   el i = indexed $ \ f m ->
     if 0 <= i && i < Seq.length m
     then f i (Seq.index m i) <&> \a -> Seq.update i a m
     else pure m
   {-# INLINE el #-}
 
-instance El Int IntMap where
+instance El IntMap where
+  type ElKey IntMap = Int
   el k = indexed $ \f m -> case IntMap.lookup k m of
      Just v -> f k v <&> \v' -> IntMap.insert k v' m
      Nothing -> pure m
   {-# INLINE el #-}
 
-instance Ord k => El k (Map k) where
+instance Ord k => El (Map k) where
+  type ElKey (Map k) = k
   el k = indexed $ \f m -> case Map.lookup k m of
      Just v  -> f k v <&> \v' -> Map.insert k v' m
      Nothing -> pure m
   {-# INLINE el #-}
 
-instance (Eq k, Hashable k) => El k (HashMap k) where
+instance (Eq k, Hashable k) => El (HashMap k) where
+  type ElKey (HashMap k) = k
   el k = indexed $ \f m -> case HashMap.lookup k m of
      Just v  -> f k v <&> \v' -> HashMap.insert k v' m
      Nothing -> pure m
@@ -124,14 +130,16 @@ instance (Eq k, Hashable k) => El k (HashMap k) where
 -- arr '!' i ≡ arr '^.' 'el' i
 -- arr '//' [(i,e)] ≡ 'el' i '.~' e '$' arr
 -- @
-instance Ix i => El i (Array i) where
+instance Ix i => El (Array i) where
+  type ElKey (Array i) = i
   el i = indexed $ \f arr ->
     if inRange (bounds arr) i
     then f i (arr Array.! i) <&> \e -> arr Array.// [(i,e)]
     else pure arr
   {-# INLINE el #-}
 
-instance El Int Vector where
+instance El Vector where
+  type ElKey Vector = Int
   el i = indexed $ \f v ->
     if 0 <= i && i < Vector.length v
     then f i (v Vector.! i) <&> \a -> v Vector.// [(i, a)]
@@ -144,7 +152,7 @@ instance El Int Vector where
 -- An instance of @At@ should satisfy:
 --
 -- @'el' k ≡ 'at' k '<.' 'traverse'@
-class El k m => At k m | m -> k where
+class El m => At m where
   -- |
   -- >>> Map.fromList [("hello","world")] ^.at "hello"
   -- Just "world"
@@ -154,9 +162,9 @@ class El k m => At k m | m -> k where
   --
   -- /Note:/ 'Map'-like containers form a reasonable instance, but not 'Array'-like ones, where
   -- you cannot satisfy the 'Lens' laws.
-  at :: k -> SimpleIndexedLens k (m v) (Maybe v)
+  at :: ElKey m -> SimpleIndexedLens (ElKey m) (m v) (Maybe v)
 
-instance At Int IntMap where
+instance At IntMap where
   at k = indexed $ \f m ->
     let mv = IntMap.lookup k m
         go Nothing   = maybe m (const (IntMap.delete k m)) mv
@@ -164,7 +172,7 @@ instance At Int IntMap where
     in go <$> f k mv where
   {-# INLINE at #-}
 
-instance Ord k => At k (Map k) where
+instance Ord k => At (Map k) where
   at k = indexed $ \f m ->
     let mv = Map.lookup k m
         go Nothing   = maybe m (const (Map.delete k m)) mv
@@ -172,7 +180,7 @@ instance Ord k => At k (Map k) where
     in go <$> f k mv
   {-# INLINE at #-}
 
-instance (Eq k, Hashable k) => At k (HashMap k) where
+instance (Eq k, Hashable k) => At (HashMap k) where
   at k = indexed $ \f m ->
     let mv = HashMap.lookup k m
         go Nothing   = maybe m (const (HashMap.delete k m)) mv
