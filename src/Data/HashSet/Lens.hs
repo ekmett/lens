0f607616b962203cae78a672823e9ec884b49ca1
Edward Kmett <ekmett@gmail.com>
2012-12-15 08:41:04 -0500

    Removed tests that will fail in the presence of hashable 1.2

--- src/Data/HashSet/Lens.hs
+++ src/Data/HashSet/Lens.hs
@@ -32,16 +32,11 @@ import Data.Hashable
 -- Sadly, you can't create a valid 'Control.Lens.Traversal.Traversal' for a 'Set', but you can
 -- manipulate it by reading using 'folded' and reindexing it via 'setmapped'.
 --
--- >>> over setmapped (+1) (fromList [1,2,3,4])
--- fromList [2,3,4,5]
 setmapped :: (Eq i, Hashable i, Eq j, Hashable j) => Setter (HashSet i) (HashSet j) i j
 setmapped = sets HashSet.map
 {-# INLINE setmapped #-}
 
 -- | Construct a set from a 'Getter', 'Control.Lens.Fold.Fold', 'Control.Lens.Traversal.Traversal', 'Control.Lens.Type.Lens' or 'Control.Lens.Iso.Iso'.
---
--- >>> setOf (folded._2) [("hello",1),("world",2),("!!!",3)]
--- fromList [1,2,3]
 --
 -- @
 -- 'setOf' :: 'Hashable' a         => 'Getter' s a           -> s -> 'HashSet' a
