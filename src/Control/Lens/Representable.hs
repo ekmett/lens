85325d8606da4f0693cf2e85e75d1ba8bab1f5a5
Edward Kmett <ekmett@gmail.com>
2012-12-21 12:49:56 -0500

    Renamed rmap to rover to get it out of the way.

--- src/Control/Lens/Representable.hs
+++ src/Control/Lens/Representable.hs
@@ -71,7 +71,7 @@ module Control.Lens.Representable
   , paths
   , tabulated
   -- * Setting with Representation
-  , rmap
+  , rover
   -- * Folding with Representation
   , rfoldMap
   , rfoldr
@@ -246,68 +246,68 @@ tabulated f = rep (f . Path)
 -- | Map over a 'Representable' functor with access to the 'Lens' for the
 -- current position
 --
--- @'rmap' f m = 'rep' '$' \\i -> f i (m '^.' i)@
-rmap :: Representable f => (Rep f -> a -> b) -> f a -> f b
-rmap f m = rep $ \i -> f i (m^.i)
-{-# INLINE rmap #-}
+-- @'rover' f m = 'rep' '$' \\i -> f i (m '^.' i)@
+rover :: Representable f => (Rep f -> a -> b) -> f a -> f b
+rover f m = rep $ \i -> f i (m^.i)
+{-# INLINE rover #-}
 
 -- | Traverse a 'Representable' functor with access to the current path
 rtraverse :: (Representable f, Traversable f, Applicative g)
           => (Rep f -> a -> g b) -> f a -> g (f b)
-rtraverse f m = sequenceA (rmap f m)
+rtraverse f m = sequenceA (rover f m)
 {-# INLINE rtraverse #-}
 
 -- | Traverse a 'Representable' functor with access to the current path
 -- as a 'Lens', discarding the result
 rtraverse_ :: (Representable f, Foldable f, Applicative g)
            => (Rep f -> a -> g b) -> f a -> g ()
-rtraverse_ f m = sequenceA_ (rmap f m)
+rtraverse_ f m = sequenceA_ (rover f m)
 {-# INLINE rtraverse_ #-}
 
 -- | Traverse a 'Representable' functor with access to the current path
 -- and a 'Lens' (and the arguments flipped)
 rfor :: (Representable f, Traversable f, Applicative g)
      => f a -> (Rep f -> a -> g b) -> g (f b)
-rfor m f = sequenceA (rmap f m)
+rfor m f = sequenceA (rover f m)
 {-# INLINE rfor #-}
 
 -- | 'mapM' over a 'Representable' functor with access to the current path
 -- as a 'Lens'
 rmapM :: (Representable f, Traversable f, Monad m)
       => (Rep f -> a -> m b) -> f a -> m (f b)
-rmapM f m = Traversable.sequence (rmap f m)
+rmapM f m = Traversable.sequence (rover f m)
 {-# INLINE rmapM #-}
 
 -- | 'mapM' over a 'Representable' functor with access to the current path
 -- as a 'Lens', discarding the result
 rmapM_ :: (Representable f, Foldable f, Monad m)
        => (Rep f -> a -> m b) -> f a -> m ()
-rmapM_ f m = Foldable.sequence_ (rmap f m)
+rmapM_ f m = Foldable.sequence_ (rover f m)
 {-# INLINE rmapM_ #-}
 
 -- | 'mapM' over a 'Representable' functor with access to the current path
 -- as a 'Lens' (with the arguments flipped)
 rforM :: (Representable f, Traversable f, Monad m)
       => f a -> (Rep f -> a -> m b) -> m (f b)
-rforM m f = Traversable.sequence (rmap f m)
+rforM m f = Traversable.sequence (rover f m)
 {-# INLINE rforM #-}
 
 -- | Fold over a 'Representable' functor with access to the current path
 -- as a 'Lens', yielding a 'Monoid'
 rfoldMap :: (Representable f, Foldable f, Monoid m)
          => (Rep f -> a -> m) -> f a -> m
-rfoldMap f m = fold (rmap f m)
+rfoldMap f m = fold (rover f m)
 {-# INLINE rfoldMap #-}
 
 -- | Fold over a 'Representable' functor with access to the current path
 -- as a 'Lens'.
 rfoldr :: (Representable f, Foldable f) => (Rep f -> a -> b -> b) -> b -> f a -> b
-rfoldr f b m = Foldable.foldr id b (rmap f m)
+rfoldr f b m = Foldable.foldr id b (rover f m)
 {-# INLINE rfoldr #-}
 
 -- | An 'IndexedSetter' that walks an 'Representable' 'Functor' using a 'Path' for an index.
 rmapped :: Representable f => IndexedSetter (Path f) (f a) (f b) a b
-rmapped f = taintedDot (rmap (\i -> untaintedDot (indexed f (Path i))))
+rmapped f = taintedDot (rover (\i -> untaintedDot (indexed f (Path i))))
 {-# INLINE rmapped #-}
 
 -- | An 'IndexedFold' that walks an 'Foldable' 'Representable' 'Functor' using a 'Path' for an index.
@@ -317,5 +317,5 @@ rfolded f =  coerce . getFolding . rfoldMap (\i -> Folding #. indexed f (Path i)
 
 -- | An 'IndexedTraversal' for a 'Traversable' 'Representable' 'Functor'.
 rtraversed :: (Representable f, Traversable f) => IndexedTraversal (Path f) (f a) (f b) a b
-rtraversed f = sequenceA . rmap (indexed f . Path)
+rtraversed f = sequenceA . rover (indexed f . Path)
 {-# INLINE rtraversed #-}
