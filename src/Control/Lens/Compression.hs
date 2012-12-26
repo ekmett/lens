89bd4ea6eb22d66cfb5e04ec0c4a1746357d6954
Shachaf Ben-Kiki <shachaf@gmail.com>
2012-12-25 22:25:16 -0800

    INLINEd everything in .Compression

--- src/Control/Lens/Compression.hs
+++ src/Control/Lens/Compression.hs
@@ -30,25 +30,30 @@ size :: Compressed a -> Int
 size (Ap s _ _ _) = s
 size Leaf{}       = 1
 size Pure         = 0
+{-# INLINE size #-}
 
 instance Functor Compressed where
   fmap f (Ap m n l r) = Ap m n (fmap f l) (fmap f r)
   fmap f (Leaf a)     = Leaf (f a)
   fmap _ Pure         = Pure
+  {-# INLINE fmap #-}
 
 instance Foldable Compressed where
   foldMap f (Ap _ _ l r) = foldMap f l `mappend` foldMap f r
   foldMap f (Leaf a)     = f a
   foldMap _ Pure         = mempty
+  {-# INLINE foldMap #-}
 
 instance Traversable Compressed where
   traverse f (Ap m n l r) = Ap m n <$> traverse f l <*> traverse f r
   traverse f (Leaf a)     = Leaf <$> f a
   traverse _ Pure         = pure Pure
+  {-# INLINE traverse #-}
 
 -- | This is an illegal Monoid.
 instance Monoid (Compressed a) where
   mempty = Pure
+  {-# INLINE mempty #-}
 
   Pure       `mappend` Pure       = Pure
   Pure       `mappend` Ap m n l r = Ap m (n + 1) l r
@@ -56,32 +61,39 @@ instance Monoid (Compressed a) where
   l@Leaf{}   `mappend` Pure       = l
   Ap m n l r `mappend` Pure       = Ap m (n + 1) l r
   l          `mappend` r          = Ap (size l + size r) 0 l r
+  {-# INLINE mappend #-}
 
 compressedIns :: Bazaar (->) a b t -> Compressed a
 compressedIns = foldMapOf (flip runBazaar) Leaf
+{-# INLINE compressedIns #-}
 
 newtype Decompression e a = Decompression { runDecompression :: Depth -> Compressed e -> a }
 
 instance Functor (Decompression e) where
   fmap f (Decompression g) = Decompression $ \ d -> f . g d
+  {-# INLINE fmap #-}
 
 -- | This is an illegal Applicative.
 instance Applicative (Decompression e) where
   pure a = Decompression $ \ _ _ -> a
+  {-# INLINE pure #-}
   Decompression mf <*> Decompression ma = Decompression $ \ d s -> case s of
     Ap _ n l r | d == n      ->          mf 0  l (ma 0  r)
     _          | d' <- d + 1 -> d' `seq` mf d' s (ma d' s)
+  {-# INLINE (<*>) #-}
 
 compressedOuts :: Bazaar (->) a b t -> Compressed b -> t
 compressedOuts bz = runDecompression go 0 where
   go = runBazaar bz $ \_ -> Decompression $ \ _ t -> case t of
     Leaf x -> x
     _      -> error "compressedOuts: wrong shape"
+{-# INLINE compressedOuts #-}
 
 -- | This is only a valid 'Lens' if you don't change the shape of the Compressed tree, etc.
 compressed :: ATraversal s t a b -> Lens s t (Compressed a) (Compressed b)
 compressed l f s = compressedOuts bz <$> f (compressedIns bz) where
   bz = l sell s
+{-# INLINE compressed #-}
 
 -- Manual evaluations some function calls to clarify what's going on:
 
