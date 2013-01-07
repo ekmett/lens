a120fdacb080a0f5e1916c230a3255ff9d890872
Edward Kmett <ekmett@gmail.com>
2013-01-06 20:30:55 -0500

    Added custom HLint.hs

---
+++ HLint.hs
@@ -0,0 +1,25 @@
+import "hint" HLint.Default
+
+-- Used hlint --find src/ in the lens repo to generate this:
+infixr 4 %%@~, <%@~, %%~, <+~, <*~, <-~, <//~, <^~, <^^~, <**~
+infix 4 %%@=, <%@=, %%=, <+=, <*=, <-=, <//=, <^=, <^^=, <**=
+infixr 2 <<~
+infixr 9 #.
+infixl 8 .#
+infixr 8 ^!, ^@!
+infixl 1 &, <&>, ??
+infixl 8 ^., ^@.
+infixr 9 <.>, <., .>
+infixr 4 %@~, .~, +~, *~, -~, //~, ^~, ^^~, **~, &&~, <>~, ||~, %~
+infix 4 %@=, .=, +=, *=, -=, //=, ^=, ^^=, **=, &&=, <>=, ||=, %=
+infixr 2 <~
+infixr 2 `zoom`, `magnify`
+infixl 8 ^.., ^?, ^?!, ^@.., ^@?, ^@?!
+infixl 8 ^#
+infixr 4 <#~, #~, #%~, <#%~, #%%~
+infix 4 <#=, #=, #%=, <#%=, #%%=
+infixl 9 :>
+infixr 4 </>~, <</>~, <.>~, <<.>~
+infix 4 </>=, <</>=, <.>=, <<.>=
+infixr 4 .|.~, .&.~, <.|.~, <.&.~
+infix 4 .|.=, .&.=, <.|.=, <.&.=
