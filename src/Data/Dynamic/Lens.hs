cfac1e2d348c09b48299f25d72655f650c0c0c26
Edward Kmett <ekmett@gmail.com>
2013-01-11 18:51:47 -0500

    Added the _Dynamic prism for working with Dynamic exceptions.

--- src/Data/Dynamic/Lens.hs
+++ src/Data/Dynamic/Lens.hs
@@ -1,24 +1,42 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Dynamic.Lens
--- Copyright   :  (C) 2012 Edward Kmett
+-- Copyright   :  (C) 2012-2013 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
--- Stability   :  provisional
--- Portability :  portable
+-- Stability   :  experimental
+-- Portability :  non-portable
 --
 ----------------------------------------------------------------------------
 module Data.Dynamic.Lens
-  ( dynamic
+  ( AsDynamic(..)
   ) where
 
+import Control.Exception
+import Control.Exception.Lens
 import Control.Lens
 import Data.Dynamic
 
--- |
--- Traverse the typed value contained in a 'Dynamic' where the type required by your function matches that
--- of the contents of the 'Dynamic'.
-dynamic :: Typeable a => Prism' Dynamic a
-dynamic = prism toDyn $ \e -> maybe (Left e) Right (fromDynamic e)
-{-# INLINE dynamic #-}
+-- | Any 'Dynamic' can be thrown as an 'Exception'
+class AsDynamic t where
+  -- | This 'Prism' allows you to traverse the typed value contained in a
+  -- 'Dynamic' where the type required by your function matches that
+  -- of the contents of the 'Dynamic', or construct a 'Dynamic' value
+  -- out of whole cloth. It can also be used to catch or throw a 'Dynamic'
+  -- value as 'SomeException'.
+  --
+  -- @
+  -- '_Dynamic' :: 'Typeable' a => 'Prism'' 'Dynamic'       a
+  -- '_Dynamic' :: 'Typeable' a => 'Prism'' 'SomeException' a
+  -- @
+  _Dynamic :: Typeable a => Prism' t a
+
+instance AsDynamic Dynamic where
+  _Dynamic = prism' toDyn fromDynamic
+  {-# INLINE _Dynamic #-}
+
+instance AsDynamic SomeException where
+  _Dynamic = exception.prism' toDyn fromDynamic
+  {-# INLINE _Dynamic #-}
