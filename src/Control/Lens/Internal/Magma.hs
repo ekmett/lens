682a51df9560e191a28c37c84a69d6940d7e2e0b
Edward Kmett <ekmett@gmail.com>
2013-01-13 20:17:56 -0500

    Committed the rest of the Magma changes

--- src/Control/Lens/Internal/Magma.hs
+++ src/Control/Lens/Internal/Magma.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
@@ -32,6 +33,7 @@ module Control.Lens.Internal.Magma
 import Control.Applicative
 import Control.Category
 import Control.Comonad
+import Control.Lens.Combinators
 import Control.Lens.Internal.Bazaar
 import Control.Lens.Internal.Context
 import Control.Lens.Internal.Indexed
@@ -55,62 +57,110 @@ class Leaf i t | t -> i where
 -- Jacket
 ------------------------------------------------------------------------------
 
-ltr :: Jacket i x b a -> Maybe x
-ltr (JacketAp _ nl _ _ _) = nl
-ltr (JacketPure x)        = Just x
-ltr (JacketLeaf _ _)      = Nothing
+-- | Extract 'Either' the 'Leftmost' key or proof that no leaves exist by collapsing the 'Jacket'.
+ltr :: Jacket i x b a -> Either i x
+ltr (JacketAp _ _ nl _ _ _) = nl
+ltr (JacketPure x)          = Right x
+ltr (JacketLeaf _ i _)      = Left i
 {-# INLINE ltr #-}
 
-rtl :: Jacket i x b a -> Maybe x
-rtl (JacketAp _ _ nr _ _) = nr
-rtl (JacketPure x)        = Just x
-rtl (JacketLeaf _ _)      = Nothing
+-- | Extract 'Either' the 'Rightmost' key or proof that no leaves exist by collapsing the 'Jacket'.
+rtl :: Jacket i x b a -> Either i x
+rtl (JacketAp _ _ _ nr _ _) = nr
+rtl (JacketPure x)          = Right x
+rtl (JacketLeaf _ i _)      = Left i
 {-# INLINE rtl #-}
 
 data Jacket i t b a where
-  JacketAp   :: Int -> Int -> Maybe y -> Maybe y -> Jacket i (x -> y) b a -> Jacket i x b a -> Jacket i y b a
+  JacketAp   :: Int -- ^ overall size
+             -> Int -- ^ prefix-sum including left branch
+             -> Either i y -- ltr with leftmost i
+             -> Either i y -- rtl with rightmost i
+             -> Jacket i (x -> y) b a
+             -> Jacket i x b a
+             -> Jacket i y b a
   JacketPure :: x -> Jacket i x b a
   JacketLeaf :: Int -> i -> a -> Jacket i b b a
 
 instance Functor (Jacket i t b) where
-  fmap f (JacketAp i j m n x y) = JacketAp i j m n (fmap f x) (fmap f y)
-  fmap _ (JacketPure x) = JacketPure x
-  fmap f (JacketLeaf i a) = JacketLeaf i (f a)
+  fmap f (JacketAp s o m n x y) = JacketAp s o m n (fmap f x) (fmap f y)
+  fmap _ (JacketPure x)         = JacketPure x
+  fmap f (JacketLeaf o i a)     = JacketLeaf o i (f a)
 
 instance Foldable (Jacket i t b) where
-  foldMap f (JacketAp _ _ _ x y)   = foldMap f x `mappend` foldMap f y
-  foldMap _ JacketPure{}     = mempty
-  foldMap f (JacketLeaf _ a) = f a
+  foldMap f (JacketAp _ _ _ _ x y) = foldMap f x `mappend` foldMap f y
+  foldMap _ JacketPure{}           = mempty
+  foldMap f (JacketLeaf _ _ a)     = f a
 
 instance Traversable (Jacket i t b) where
-  traverse f (JacketAp i m n x y) = JacketAp i m n <$> traverse f x <*> traverse f y
+  traverse f (JacketAp s o m n x y) = JacketAp s o m n <$> traverse f x <*> traverse f y
   traverse _ (JacketPure x) = pure (JacketPure x)
-  traverse f (JacketLeaf i a) = JacketLeaf i <$> f a
-
-instance b ~ t => Leaf i (Jacket i t b) where
-  leaf = JacketLeaf
+  traverse f (JacketLeaf o i a) = JacketLeaf o i <$> f a
 
 instance (Show i, Show a) => Show (Jacket i t b a) where
-  showsPrec d (JacketAp _ _ _ x y) = showParen (d > 4) $
+  showsPrec d (JacketAp _ _ _ _ x y) = showParen (d > 4) $
     showsPrec 4 x . showString " <*> " . showsPrec 5 y
   showsPrec d (JacketPure _) = showParen (d > 10) $
     showString "pure .."
-  showsPrec d (JacketLeaf i a) = showParen (d > 10) $
+  showsPrec d (JacketLeaf _ i a) = showParen (d > 10) $
     showString "leaf " . showsPrec 11 i . showChar ' ' . showsPrec 11 a
 
 ------------------------------------------------------------------------------
 -- Tailor
 ------------------------------------------------------------------------------
 
-newtype Tailor i a b t = Tailor Int (Int -> Jacket i t b a)
+data Tailor i a b t = Tailor Int (Int -> Jacket i t b a)
 
 instance Functor (Tailor i a b) where
-  fmap f (Tailor w k) = Tailor w $ \o -> let ko = k o in JacketAp m (ltr ko) (rtl ko) (JacketPure f) ko
+  fmap f (Tailor w k) = Tailor w $ \o -> let ko = k o in JacketAp w o (f <$> ltr ko) (f <$> rtl ko) (JacketPure f) ko
+  {-# INLINE fmap #-}
 
-instance Applicative (Molten i a b) where
-  pure a = Tailor 0 $ \_ -> Pure a
-  Molten xs <*> Molten ys = Molten (ScoriaAp xs ys)
+instance Applicative (Tailor i a b) where
+  pure a = Tailor 0 $ \_ -> JacketPure a
+  {-# INLINE pure #-}
+  Tailor wf mf <*> Tailor wa ma = Tailor wfa $ \o -> let
+      p = o + wf
+      mfo = mf o
+      mao = ma p
+    in JacketAp wfa p (ltr mfo <*> ltr mao) ((&) <$> rtl mao <*> rtl mfo) mfo mao
+    where wfa = wf + wa
+  {-# INLINE (<*>) #-}
 
+instance (p ~ Indexed i) => Sellable p (Tailor i) where
+  sell = Indexed $ \i a -> Tailor 1 $ \ o -> JacketLeaf o i a
+  {-# INLINE sell #-}
+
+instance Indexable i p => Bizarre p (Tailor i) where
+  bazaar (pafb :: p a (f b)) (Tailor _ k) = go (k 0) where
+    go :: Applicative f => Jacket i t b a -> f t
+    go (JacketAp _ _ _ _ x y) = go x <*> go y
+    go (JacketPure x)         = pure x
+    go (JacketLeaf _ i a)     = indexed pafb i a
+  {-# INLINE bazaar #-}
+
+instance IndexedFunctor (Tailor i) where
+  ifmap f (Tailor w k) = Tailor w $ \o -> let ko = k o in JacketAp w o (f <$> ltr ko) (f <$> rtl ko) (JacketPure f) ko
+  {-# INLINE ifmap #-}
+
+{-
+instance IndexedComonad (Tailor i) where
+  iextract (Tailor _ k) = go (k 0) where
+    go :: Jacket i t a a -> t
+    go (JacketAp _ _ _ _ x y) = go x (go y)
+    go (JacketPure x)         = x
+    go (JacketLeaf _ _ a)     = a
+  {-# INLINE iextract #-}
+
+  iduplicate (Tailor w k) = Tailor w (go . k) where
+    go :: Jacket i t b a -> Jacket i (Tailor i x b t) x a
+    go (JacketPure x)     = JacketPure (pure x)
+    ...
+
+instance a ~ b => Comonad (Tailor i a b) where
+  extract   = iextract
+  extend    = iextend
+  duplicate = iduplicate
+-}
 
 ------------------------------------------------------------------------------
 -- Scoria
@@ -123,10 +173,10 @@ data Scoria i t b a where
   ScoriaLeaf :: i -> a -> Scoria i b b a
 
 instance Functor (Scoria i t b) where
-  fmap f (ScoriaAp x y) = ScoriaAp (fmap f x) (fmap f y)
-  fmap _ (ScoriaPure x) = ScoriaPure x
+  fmap f (ScoriaAp x y)    = ScoriaAp (fmap f x) (fmap f y)
+  fmap _ (ScoriaPure x)    = ScoriaPure x
   fmap f (ScoriaFmap xy x) = ScoriaFmap xy (fmap f x)
-  fmap f (ScoriaLeaf i a) = ScoriaLeaf i (f a)
+  fmap f (ScoriaLeaf i a)  = ScoriaLeaf i (f a)
 
 instance Foldable (Scoria i t b) where
   foldMap f (ScoriaAp x y)   = foldMap f x `mappend` foldMap f y
@@ -135,10 +185,10 @@ instance Foldable (Scoria i t b) where
   foldMap f (ScoriaLeaf _ a) = f a
 
 instance Traversable (Scoria i t b) where
-  traverse f (ScoriaAp x y) = ScoriaAp <$> traverse f x <*> traverse f y
-  traverse _ (ScoriaPure x) = pure (ScoriaPure x)
+  traverse f (ScoriaAp x y)    = ScoriaAp <$> traverse f x <*> traverse f y
+  traverse _ (ScoriaPure x)    = pure (ScoriaPure x)
   traverse f (ScoriaFmap xy x) = ScoriaFmap xy <$> traverse f x
-  traverse f (ScoriaLeaf i a) = ScoriaLeaf i <$> f a
+  traverse f (ScoriaLeaf i a)  = ScoriaLeaf i <$> f a
 
 instance b ~ t => Leaf i (Scoria i t b) where
   leaf = ScoriaLeaf
