9073ed474279af5dc3cbe8e5591b77a73229ac99
Edward Kmett <ekmett@gmail.com>
2013-01-16 00:47:30 -0500

    Continuing exploration of Control.Lens.Internal.While

--- src/Control/Lens/Internal/While.hs
+++ src/Control/Lens/Internal/While.hs
@@ -49,45 +49,45 @@ import Prelude hiding ((.),id)
 -- While
 ------------------------------------------------------------------------------
 
-data While p t b a where
-  WhileAp   :: Bool -> While p (x -> y) b a -> While p x b a -> While p y b a
-  WhilePure :: t -> While p t b a
-  WhileFmap :: (x -> y) -> While p x b a -> While p y b a
-  WhileLeaf :: Bool -> Corep p a -> While p b b a
+data While t b a where
+  WhileAp   :: Bool -> While (x -> y) b a -> While x b a -> While y b a
+  WhilePure :: t -> While t b a
+  WhileFmap :: (x -> y) -> While x b a -> While y b a
+  WhileLeaf :: Bool -> a -> While b b a
 
-instance Corepresentable p => Functor (While p t b) where
+instance Functor (While t b) where
   fmap f (WhileAp b x y)  = WhileAp b (fmap f x) (fmap f y)
   fmap _ (WhilePure x)    = WhilePure x
   fmap f (WhileFmap xy x) = WhileFmap xy (fmap f x)
-  fmap f (WhileLeaf b wa) = WhileLeaf b (fmap f wa)
+  fmap f (WhileLeaf b a) = WhileLeaf b (f a)
 
-instance (Corepresentable p, Foldable (Corep p)) => Foldable (While p t b) where
+instance Foldable (While t b) where
   foldMap f (WhileAp b x y)  = foldMap f x `mappend` foldMap f y
   foldMap _ WhilePure{}      = mempty
   foldMap f (WhileFmap _ x)  = foldMap f x
-  foldMap f (WhileLeaf _ wa) = foldMap f wa
+  foldMap f (WhileLeaf _ a)  = f a
 
-instance (Corepresentable p, Traversable (Corep p)) => Traversable (While p t b) where
+instance Traversable (While t b) where
   traverse f (WhileAp b x y)  = WhileAp b <$> traverse f x <*> traverse f y
   traverse _ (WhilePure x)    = pure (WhilePure x)
   traverse f (WhileFmap xy x) = WhileFmap xy <$> traverse f x
-  traverse f (WhileLeaf b wa) = WhileLeaf b <$> traverse f wa
+  traverse f (WhileLeaf b a)  = WhileLeaf b <$> f a
 
-instance (Show (Corep p a), Show a) => Show (While p t b a) where
+instance Show a => Show (While t b a) where
   showsPrec d (WhileAp _ x y) = showParen (d > 4) $
     showsPrec 4 x . showString " <*> " . showsPrec 5 y
   showsPrec d (WhilePure _) = showParen (d > 10) $
     showString "pure .."
   showsPrec d (WhileFmap _ x) = showParen (d > 4) $
     showString ".. <$> " . showsPrec 5 x
-  showsPrec d (WhileLeaf b wa) = showParen (d > 10) $
-    showString "while " . showsPrec 11 b . showChar ' ' . showsPrec 11 wa
+  showsPrec d (WhileLeaf b a) = showParen (d > 10) $
+    showString "while " . showsPrec 11 b . showChar ' ' . showsPrec 11 a
 
-runWhile :: (Corepresentable p, Comonad (Corep p)) => While p t a a -> t
-runWhile (WhileAp _ l r)  = runWhile l (runWhile r)
-runWhile (WhileFmap f r)  = f (runWhile r)
-runWhile (WhilePure x)    = x
-runWhile (WhileLeaf _ wa) = extract wa
+runWhile :: While t a a -> t
+runWhile (WhileAp _ l r) = runWhile l (runWhile r)
+runWhile (WhileFmap f r) = f (runWhile r)
+runWhile (WhilePure x)   = x
+runWhile (WhileLeaf _ a) = a
 
 ------------------------------------------------------------------------------
 -- TakingWhile
@@ -96,9 +96,9 @@ runWhile (WhileLeaf _ wa) = extract wa
 -- | This is used to generate an indexed magma from an unindexed source
 --
 -- By constructing it this way we avoid infinite reassociations in sums where possible.
-data TakingWhile p (f :: * -> *) a b t = TakingWhile Bool (Bool -> While p t b a)
+data TakingWhile p (f :: * -> *) a b t = TakingWhile Bool (Bool -> While t b (Corep p a))
 
-runTakingWhile :: TakingWhile p f a b t -> While p t b a
+runTakingWhile :: Corepresentable p => TakingWhile p f a b t -> While t b (Corep p a)
 runTakingWhile (TakingWhile _ k) = k True
 
 instance Functor (TakingWhile p f a b) where
@@ -113,7 +113,7 @@ instance (a ~ b) => Applicative (TakingWhile p f a b) where
 
 instance Corepresentable p => Bizarre p (TakingWhile p g) where
   bazaar (pafb :: p a (f b)) (TakingWhile _ k) = go (k True) where
-    go :: Applicative f => While p t b a -> f t
+    go :: Applicative f => While t b (Corep p a) -> f t
     go (WhileAp _ x y)  = go x <*> go y
     go (WhileFmap f x)  = f <$> go x
     go (WhilePure x)    = pure x
