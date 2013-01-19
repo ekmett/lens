7fc29e8910703feeb0c48c57855de5382f8f5534
Edward Kmett <ekmett@gmail.com>
2013-01-19 05:37:04 -0500

    Revert "an attempt to make travis error on doctest failure"
    This reverts commit b558cf52d073478439448db15f90a0462860e404.

--- src/Control/Lens/At.hs
+++ src/Control/Lens/At.hs
@@ -68,32 +68,18 @@ import Data.Vector.Storable as Storable
 -- >>> let g :: Expr -> Expr; g = Debug.SimpleReflect.Vars.g
 
 -- | A deprecated alias for 'ix'.
-_at, resultAt :: Ixed f m => IxKey m -> IndexedLensLike' (IxKey m) f m (IxValue m)
+contains, _at, resultAt :: Ixed f m => IxKey m -> IndexedLensLike' (IxKey m) f m (IxValue m)
+contains = ix
 _at      = ix
 resultAt = ix
 {-# DEPRECATED _at, contains, resultAt "use 'ix'. This function will be removed in version 3.9" #-}
-
-type family ContainsKey (m :: *) :: *
-
-class Functor f => Contains f m where
-  -- |
-  -- >>> IntSet.fromList [1,2,3,4] ^. contains 3
-  -- True
-  --
-  -- >>> IntSet.fromList [1,2,3,4] ^. contains 5
-  -- False
-  --
-  -- >>> IntSet.fromList [1,2,3,4] & contains 3 .~ False
-  -- fromList [1,2,4]
-  contains :: ContainsKey m -> IndexedLensLike' (ContainsKey m) f m Bool
-
 
 type family IxKey (m :: *) :: *
 type family IxValue (m :: *) :: *
 
 -- | This simple 'IndexedTraversal' lets you 'traverse' the value at a given
 -- key in a 'Map' or element at an ordinal position in a list or 'Seq'.
-class Functor f => Ixed f m where
+class Ixed f m where
   -- | This simple 'IndexedTraversal' lets you 'traverse' the value at a given
   -- key in a 'Map' or element at an ordinal position in a list or 'Seq'.
   --
@@ -114,6 +100,14 @@ class Functor f => Ixed f m where
   -- >>> Seq.fromList [] ^? ix 2
   -- Nothing
   --
+  -- >>> IntSet.fromList [1,2,3,4] & ix 3 .~ False
+  -- fromList [1,2,4]
+  --
+  -- >>> IntSet.fromList [1,2,3,4] ^. ix 3
+  -- True
+
+  -- >>> IntSet.fromList [1,2,3,4] ^. ix 5
+  -- False
   ix :: IxKey m -> IndexedLensLike' (IxKey m) f m (IxValue m)
 #ifdef DEFAULT_SIGNATURES
   default ix :: (Applicative f, At m) => IxKey m -> IndexedLensLike' (IxKey m) f m (IxValue m)
