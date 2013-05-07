84a9ec3ae5c548ef088ae37a2bf28f029056646f
Alexander Altman <alexanderaltman@me.com>
2013-05-07 15:36:01 -0700

    Added `Control.Monad.Primitive.Lens`
    [none]

---
+++ src/Control/Monad/Primitive/Lens.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE TypeFamilies #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Primitive.Lens
+-- Copyright   :  (C) 2013 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  Control.Monad.Primitive
+--
+----------------------------------------------------------------------------
+module Control.Monad.Primitive.Lens
+  (
+    prim
+  ) where
+
+import Control.Lens
+import Control.Monad.Primitive (PrimMonad(..))
+import GHC.Prim (State#)
+
+prim :: (PrimMonad m) => Iso' (m a) (State# (PrimState m) -> (# State# (PrimState m), a #))
+prim = iso internal primitive
+{-# INLINE prim #-}
