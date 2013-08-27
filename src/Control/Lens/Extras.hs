b53c2be00b56c119239300253c7b9f64e647a5f5
nand <nand@lavabit.com>
2013-08-27 20:47:42 +0200

    Add 'is' in new module Control.Lens.Extras

---
+++ src/Control/Lens/Extras.hs
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.List.Lens
+-- Copyright   :  (C) 2012-13 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- A few extra names that didn't make it into Control.Lens.
+--
+----------------------------------------------------------------------------
+module Control.Lens.Extras
+  ( is
+  , module Data.Data.Lens
+  ) where
+
+import Control.Lens
+import Data.Data.Lens
+
+-- | Check to see if this 'Prism' matches.
+--
+-- >>> is _Left (Right 12)
+-- False
+--
+-- >>> is hex "3f79"
+-- True
+is :: APrism s t a b -> s -> Bool
+is k = not . isn't k
+{-# INLINE is #-}
