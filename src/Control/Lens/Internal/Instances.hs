75ad3ca679e8ca9af730701994e6ad92c64a5ac5
Edward Kmett <ekmett@gmail.com>
2013-12-24 01:48:50 -0500

    base 4.7 compatibility

--- src/Control/Lens/Internal/Instances.hs
+++ src/Control/Lens/Internal/Instances.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+
+#ifndef MIN_VERSION_base
+#define MIN_VERSION_base(x,y,z) 1
+#endif
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Lens.Internal.Instances
@@ -14,27 +19,25 @@
 module Control.Lens.Internal.Instances () where
 
 import Control.Applicative
-import Data.Foldable
-import Data.Monoid
 import Data.Semigroup.Foldable
 import Data.Semigroup.Traversable
+
+#if !(MIN_VERSION_base(4,7,0))
+import Data.Monoid
+import Data.Foldable
 import Data.Traversable
+#endif
 
 -------------------------------------------------------------------------------
 -- Orphan Instances
 -------------------------------------------------------------------------------
 
+#if !(MIN_VERSION_base(4,7,0))
 instance Foldable ((,) b) where
   foldMap f (_, a) = f a
 
-instance Foldable1 ((,) b) where
-  foldMap1 f (_, a) = f a
-
 instance Traversable ((,) b) where
   traverse f (b, a) = (,) b <$> f a
-
-instance Traversable1 ((,) b) where
-  traverse1 f (b, a) = (,) b <$> f a
 
 instance Foldable (Either a) where
   foldMap _ (Left _) = mempty
@@ -49,3 +52,11 @@ instance Foldable (Const m) where
 
 instance Traversable (Const m) where
   traverse _ (Const m) = pure $ Const m
+#endif
+
+instance Foldable1 ((,) b) where
+  foldMap1 f (_, a) = f a
+
+instance Traversable1 ((,) b) where
+  traverse1 f (b, a) = (,) b <$> f a
+
