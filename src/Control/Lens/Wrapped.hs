2b3f10a7ca2739a4205a3f5e1c21bd122c597f53
Edward Kmett <ekmett@gmail.com>
2014-06-01 15:55:56 -0400

    no orphans

--- src/Control/Lens/Wrapped.hs
+++ src/Control/Lens/Wrapped.hs
@@ -12,7 +12,7 @@
 #ifdef TRUSTWORTHY
 {-# LANGUAGE Trustworthy #-}
 #endif
-{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-warnings-deprecations #-}
+{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Lens.Wrapped
