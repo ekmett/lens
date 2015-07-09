f4f0feb6fda0055d03787f7d871badb5257c7bfe
Edward Kmett <ekmett@gmail.com>
2015-07-08 22:31:32 -0400

    finish removing unnecessary redundant constraint warnings

--- src/Control/Lens/At.hs
+++ src/Control/Lens/At.hs
@@ -14,6 +14,10 @@
 {-# LANGUAGE Trustworthy #-}
 #endif
 
+#if __GLASGOW_HASKELL__ >= 711
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+#endif
+
 #ifndef MIN_VERSION_base
 #define MIN_VERSION_base(x,y,z) 1
 #endif
