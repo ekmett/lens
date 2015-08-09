5b298fc2db829197403cea49ab936a336935a2f9
Edward Kmett <ekmett@gmail.com>
2015-08-09 17:38:30 -0400

    fix traverseOf

--- src/Control/Lens/Traversal.hs
+++ src/Control/Lens/Traversal.hs
@@ -272,7 +272,7 @@ type Traversing1' p f s a = Traversing1 p f s s a a
 -- 'traverseOf' :: 'Functor' f => 'Lens' s t a b      -> (a -> f b) -> s -> f t
 -- 'traverseOf' :: 'Applicative' f => 'Traversal' s t a b -> (a -> f b) -> s -> f t
 -- @
-traverseOf :: Over p f s t a b -> (a -> f b) -> s -> f t
+traverseOf :: LensLike f s t a b -> (a -> f b) -> s -> f t
 traverseOf = id
 {-# INLINE traverseOf #-}
 
