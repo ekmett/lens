67ac5db4ee24364c435e6e9fbe29fe429bce8d0c
Eric Mertens <emertens@gmail.com>
2016-06-15 11:20:36 -0700

    Update hackage-docs.sh for new cabal-install
    [ci skip]

--- scripts/hackage-docs.sh
+++ scripts/hackage-docs.sh
@@ -1,56 +1,17 @@
 #!/bin/bash
+
+# Run this script in the top-level of your package directory
+# (where the .cabal file is) to compile documentation and
+# upload it to hackage.
+
+# Requirements:
+# cabal-install-1.24 (for --for-hackage)
+# haddock 2.17 (for the hyperlinked source)
+
 set -e
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: scripts/hackage-docs.sh HACKAGE_USER"
-  exit 1
-fi
-
-user=$1
-
-cabal_file=$(find . -maxdepth 1 -name "*.cabal" -print -quit)
-if [ ! -f "$cabal_file" ]; then
-  echo "Run this script in the top-level package directory"
-  exit 1
-fi
-
-pkg=$(awk -F ":[[:space:]]*" 'tolower($1)=="name"    { print $2 }' < "$cabal_file")
-ver=$(awk -F ":[[:space:]]*" 'tolower($1)=="version" { print $2 }' < "$cabal_file")
-
-if [ -z "$pkg" ]; then
-  echo "Unable to determine package name"
-  exit 1
-fi
-
-if [ -z "$ver" ]; then
-  echo "Unable to determine package version"
-  exit 1
-fi
-
-echo "Detected package: $pkg-$ver"
-
-dir=$(mktemp -d build-docs.XXXXXX)
+dir=$(mktemp -d dist-docs.XXXXXX)
 trap 'rm -r "$dir"' EXIT
 
-
-if haddock --hyperlinked-source >/dev/null
-then
-  echo "Using fancy hyperlinked source"
-  HYPERLINK_FLAG="--haddock-option=--hyperlinked-source"
-else
-  echo "Using boring hyperlinked source"
-  HYPERLINK_FLAG="--hyperlink-source"
-fi
-
-cabal haddock --hoogle $HYPERLINK_FLAG --html-location='/package/$pkg-$version/docs' --contents-location='/package/$pkg-$version'
-
-cp -R dist/doc/html/$pkg/ $dir/$pkg-$ver-docs
-
-tar cvz -C $dir --format=ustar -f $dir/$pkg-$ver-docs.tar.gz $pkg-$ver-docs
-
-curl -X PUT \
-     -H 'Content-Type: application/x-tar' \
-     -H 'Content-Encoding: gzip' \
-     -u "$user" \
-     --data-binary "@$dir/$pkg-$ver-docs.tar.gz" \
-     "https://hackage.haskell.org/package/$pkg-$ver/docs"
+cabal haddock --builddir="$dir" --for-hackage --haddock-option=--hyperlinked-source
+cabal upload -d $dir/*-docs.tar.gz
