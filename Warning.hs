7031e00f62a704a86c3149c75c1e2afc059af022
Ryan Scott <ryan.gl.scott@gmail.com>
2017-05-12 11:48:03 -0400

    Simplify the Setup script
    See https://github.com/phadej/cabal-doctest/issues/11

---
+++ Warning.hs
@@ -0,0 +1,5 @@
+module Warning
+  {-# WARNING ["You are configuring this package without cabal-doctest installed.",
+               "The doctests test-suite will not work as a result.",
+               "To fix this, install cabal-doctest before configuring."] #-}
+  () where
