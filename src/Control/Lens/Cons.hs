1c1295fdde342ff6f3446fd281bf3018a05b9ceb
ncaq <ncaq@ncaq.net>
2024-03-06 23:25:54 +0900

    refactor: move: Cons like operator from Setter to Cons
    What
    ===
    
    Move `(<|~)`, `(<|=)`, `(|>~)`, `(|>=)` from `Control.Lens.Setter` to `Control.Lens.Cons`.
    
    Why
    ===
    
    I try impl with result variants to `Control.Lens.Lens` like other.
    This requires the import of `Control.Lens.Cons` in `Control.Lens.Lens`.
    But it cause cycle import.
    So I decided to implement Cons-related operators with `Control.Lens.Cons`.

--- src/Control/Lens/Cons.hs
+++ src/Control/Lens/Cons.hs
@@ -27,6 +27,7 @@ module Control.Lens.Cons
   , cons
   , uncons
   , _head, _tail
+  , (<|~), (<|=)
   , pattern (:<)
   -- * Snoc
   , Snoc(..)
@@ -34,6 +35,7 @@ module Control.Lens.Cons
   , snoc
   , unsnoc
   , _init, _last
+  , (|>~), (|>=)
   , pattern (:>)
 
   ) where
@@ -42,6 +44,7 @@ import Control.Lens.Equality (simply)
 import Control.Lens.Fold
 import Control.Lens.Prism
 import Control.Lens.Review
+import Control.Lens.Setter
 import Control.Lens.Tuple
 import Control.Lens.Type
 import qualified Data.ByteString      as StrictB
@@ -62,6 +65,7 @@ import           Data.Vector.Unboxed (Unbox)
 import qualified Data.Vector.Unboxed as Unbox
 import           Data.Word
 import           Control.Applicative (ZipList(..))
+import           Control.Monad.State.Class as State
 import           Prelude
 
 -- $setup
@@ -77,6 +81,8 @@ import           Prelude
 
 infixr 5 <|, `cons`
 infixl 5 |>, `snoc`
+infixr 4 <|~, |>~
+infix  4 <|=, |>=
 
 pattern (:<) :: Cons b b a a => a -> b -> b
 pattern (:<) a s <- (preview _Cons -> Just (a,s)) where
@@ -323,6 +329,19 @@ _tail :: Cons s s a a => Traversal' s s
 _tail = _Cons._2
 {-# INLINE _tail #-}
 
+-- | Modify the target of a 'Cons' value by using @('<|')@.
+--
+-- >>> (["world"], ["lens"]) & _1 <|~ "hello"
+-- (["hello","world"],["lens"])
+(<|~) :: Cons b b a a => ASetter s t b b -> a -> s -> t
+l <|~ n = over l (n <|)
+{-# INLINE (<|~) #-}
+
+-- | Modify the target(s) of a 'Lens'', 'Iso', 'Setter' or 'Traversal' by using @('<|')@.
+(<|=) :: (MonadState s m, Cons b b a a) => ASetter s s b b -> a -> m ()
+l <|= a = State.modify (l <|~ a)
+{-# INLINE (<|=) #-}
+
 ------------------------------------------------------------------------------
 -- Snoc
 ------------------------------------------------------------------------------
@@ -538,3 +557,16 @@ snoc = curry (simply review _Snoc)
 unsnoc :: Snoc s s a a => s -> Maybe (s, a)
 unsnoc = simply preview _Snoc
 {-# INLINE unsnoc #-}
+
+-- | Modify the target of a 'Cons' value by using @('|>')@.
+--
+-- >>> (["world"], ["lens"]) & _1 |>~ "hello"
+-- (["world","hello"],["lens"])
+(|>~) :: Snoc b b a a => ASetter s t b b -> a -> s -> t
+l |>~ n = over l (|> n)
+{-# INLINE (|>~) #-}
+
+-- | Modify the target(s) of a 'Lens'', 'Iso', 'Setter' or 'Traversal' by using @('|>')@.
+(|>=) :: (MonadState s m, Snoc b b a a) => ASetter s s b b -> a -> m ()
+l |>= a = State.modify (l |>~ a)
+{-# INLINE (|>=) #-}
