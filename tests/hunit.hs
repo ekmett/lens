71b25e9798686095bf993bc37531856c48e27d36
ncaq <ncaq@ncaq.net>
2024-03-07 00:18:43 +0900

    feat: add: `<<|~`, `<<|=`, `<|>~`, `<|>=`
    Add Cons operators of "with result" variants.

--- tests/hunit.hs
+++ tests/hunit.hs
@@ -261,6 +261,16 @@ case_cons_to_state_record_field = do
     test = points <|= origin
     trig' = trig { _points = origin : (trig & _points) }
 
+case_cons_to_record_field_and_access_new_value =
+  (trig & points <<|~ origin)
+    @?= (origin : _points trig, trig { _points = origin : (trig & _points) })
+
+case_cons_to_state_record_field_and_access_new_value =
+  runState test trig @?= ([ origin ] <> _points trig, trig')
+  where
+    test = points <<|= origin
+    trig' = trig { _points = origin : (trig & _points) }
+
 case_snoc_to_record_field =
   (trig & points |>~ origin)
     @?= trig { _points = (trig & _points) `snoc` origin }
@@ -269,6 +279,16 @@ case_snoc_to_state_record_field = do
   runState test trig @?= ((), trig')
   where
     test = points |>= origin
+    trig' = trig { _points = (trig & _points) `snoc` origin }
+
+case_snoc_to_record_field_and_access_new_value =
+  (trig & points <|>~ origin)
+    @?= (_points trig `snoc` origin, trig { _points = (trig & _points) `snoc` origin })
+
+case_snoc_to_state_record_field_and_access_new_value =
+  runState test trig @?= (_points trig <> [ origin ], trig')
+  where
+    test = points <|>= origin
     trig' = trig { _points = (trig & _points) `snoc` origin }
 
 case_append_to_record_field_and_access_old_value =
@@ -373,6 +393,10 @@ main = defaultMain
     , testCase "append to state record field and access new value" case_append_to_state_record_field_and_access_new_value
     , testCase "prepend to record field and access new value" case_prepend_to_record_field_and_access_new_value
     , testCase "prepend to state record field and access new value" case_prepend_to_state_record_field_and_access_new_value
+    , testCase "cons to record field and access new value" case_cons_to_record_field_and_access_new_value
+    , testCase "cons to state record field and access new value" case_cons_to_state_record_field_and_access_new_value
+    , testCase "snoc to record field and access new value" case_snoc_to_record_field_and_access_new_value
+    , testCase "snoc to state record field and access new value" case_snoc_to_state_record_field_and_access_new_value
     , testCase "append to record field and access old value" case_append_to_record_field_and_access_old_value
     , testCase "append to state record field and access old value" case_append_to_state_record_field_and_access_old_value
     , testCase "read maybe map entry" case_read_maybe_map_entry
