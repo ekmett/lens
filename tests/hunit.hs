26bdaac49279c35a940817d6c1ad091b742537a7
Ryan Scott <ryan.gl.scott@gmail.com>
2024-05-12 11:31:29 -0400

    Define variants of #1058 operators which return old result
    As requested in
    https://github.com/ekmett/lens/pull/1068#pullrequestreview-2051269100.

--- tests/hunit.hs
+++ tests/hunit.hs
@@ -292,14 +292,34 @@ case_snoc_to_state_record_field_and_access_new_value =
     trig' = trig { _points = (trig & _points) `snoc` origin }
 
 case_append_to_record_field_and_access_old_value =
-  (trig & points <<%~ (<>[origin]))
+  (trig & points <<<>~ [ origin ])
     @?= (_points trig, trig { _points = (trig & _points) <> [ origin ] })
 
 case_append_to_state_record_field_and_access_old_value = do
   runState test trig @?= (_points trig, trig')
   where
-    test = points <<%= (<>[origin])
+    test = points <<<>= [ origin ]
     trig' = trig { _points = (trig & _points) <> [ origin ] }
+
+case_cons_to_record_field_and_access_old_value =
+  (trig & points <<<|~ origin)
+    @?= (_points trig, trig { _points = origin : (trig & _points) })
+
+case_cons_to_state_record_field_and_access_old_value =
+  runState test trig @?= (_points trig, trig')
+  where
+    test = points <<<|= origin
+    trig' = trig { _points = origin : (trig & _points) }
+
+case_snoc_to_record_field_and_access_old_value =
+  (trig & points <<|>~ origin)
+    @?= (_points trig, trig { _points = (trig & _points) `snoc` origin })
+
+case_snoc_to_state_record_field_and_access_old_value =
+  runState test trig @?= (_points trig, trig')
+  where
+    test = points <<|>= origin
+    trig' = trig { _points = (trig & _points) `snoc` origin }
 
 case_read_maybe_map_entry = trig^.labels.at origin @?= Just "Origin"
 
@@ -399,6 +419,10 @@ main = defaultMain
     , testCase "snoc to state record field and access new value" case_snoc_to_state_record_field_and_access_new_value
     , testCase "append to record field and access old value" case_append_to_record_field_and_access_old_value
     , testCase "append to state record field and access old value" case_append_to_state_record_field_and_access_old_value
+    , testCase "cons to record field and access old value" case_cons_to_record_field_and_access_old_value
+    , testCase "cons to state record field and access old value" case_cons_to_state_record_field_and_access_old_value
+    , testCase "snoc to record field and access old value" case_snoc_to_record_field_and_access_old_value
+    , testCase "snoc to state record field and access old value" case_snoc_to_state_record_field_and_access_old_value
     , testCase "read maybe map entry" case_read_maybe_map_entry
     , testCase "read maybe state map entry" case_read_maybe_state_map_entry
     , testCase "read map entry" case_read_map_entry
